d2b62d1362b9fbe776b523cd8aaed3baa8661910
kdrcnuzmcu <kdrcn.uzmcu@gmail.com>
2022-04-02 00:07:36 +0300

    Update form-namaz-vakitleri.py

--- form-namaz-vakitleri.py
+++ form-namaz-vakitleri.py
@@ -1,67 +1,81 @@
 import tkinter as tk
 from tkinter import messagebox
-from flask import request_started 
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import locale
+
+lbl = []
+lbl_ = []
+
+lblA = []
+lbl_A = []
 
 def yenile():
-    url = "https://namazvakitleri.diyanet.gov.tr/tr-TR/9638/kirklareli-icin-namaz-vakti"
+    url1 = "https://namazvakitleri.diyanet.gov.tr/tr-TR/9638/kirklareli-icin-namaz-vakti"
+    url2 = "https://www.accuweather.com/tr/tr/kirklareli/318684/weather-forecast/318684"
+    url3 = "https://www.accuweather.com/tr/tr/kirklareli/318684/daily-weather-forecast/318684"
     try:
-        r = requests.get(url)
+        r = requests.get(url1)
         bs = BeautifulSoup(r.text, "html.parser")
-
-        lbl11["text"] = ":  " + bs.find_all('div', class_='tpt-time')[1].get_text()
-        lbl22["text"] = ":  " + bs.find_all('div', class_='tpt-time')[2].get_text()
-        lbl33["text"] = ":  " + bs.find_all('div', class_='tpt-time')[3].get_text()
-        lbl44["text"] = ":  " + bs.find_all('div', class_='tpt-time')[4].get_text()
-        lbl55["text"] = ":  " + bs.find_all('div', class_='tpt-time')[5].get_text()
-        lbl66["text"] = ":  " + bs.find_all('div', class_='tpt-time')[6].get_text()
+        for i in range(6):
+            lbl_[i]["text"] = bs.find_all('div', class_='tpt-time')[i+1].get_text()
+        """   
+        lbl11["text"] = bs.find_all('div', class_='tpt-time')[1].get_text()
+        
+        r = requests.get(url2)
+        bs = BeautifulSoup(r.text, "html.parser")
+        print(bs.find_all('div', class_ = 'temp').get_text())
+        # lbl_A[0]["text"] = bs.find_all('div', class_ = 'temp').get_text()
+        """ 
     except:
         tk.messagebox.showerror(title="Hata!", message="Veriler alınamadı.")
     # print(r.status_code)
     # vakitler = bs.find_all('div', class_='tpt-time')[].get_text()
-    
+
+# ----- ----- ----- ----- ----- PENCERE ÖZELLİKLERİ ----- ----- ----- ----- ----- 
+
 window = tk.Tk()
-window.title = "Namaz Vakitleri"
-window.geometry("200x225")
 window.resizable(width = False, height = False)
+window.title("Merkez/Kırklareli")
 
-lbl_tarih = tk.Label(master = window, text = "---")
+# ----- ----- ----- ----- ----- TARİH VE KONUM ----- ----- ----- ----- -----
 
-frm_vakitler = tk.Frame(master = window, relief = "ridge", borderwidth = 3)
+locale.setlocale(locale.LC_TIME, "tr_TR") # TURKEY
+lbl_saat = tk.Label(master = window, text = datetime.today().strftime("%d / %B / %Y %A - %X"))
+lbl_saat.grid(row = 0, column = 0, padx = 10)
 
-lbl1 = tk.Label(master = frm_vakitler, text = "İmsak", font = "20")
-lbl2 = tk.Label(master = frm_vakitler, text = "Sabah", font = "20")
-lbl3 = tk.Label(master = frm_vakitler, text = "Öğle", font = "20")
-lbl4 = tk.Label(master = frm_vakitler, text = "İkindi", font = "20")
-lbl5 = tk.Label(master = frm_vakitler, text = "Akşam", font = "20")
-lbl6 = tk.Label(master = frm_vakitler, text = "Yatsı", font = "20")
+# ----- ----- ----- ----- ----- NAMAZ VAKİTLERİ ----- ----- ----- ----- -----
+frm_namaz = tk.Frame(master = window, relief = "ridge", borderwidth = 3)
+frm_namaz.grid(row = 1, column = 0, padx = 10, pady = 10)
+vakitler = ["İmsak:", "Sabah:", "Öğle:", "İkindi:", "Akşam:", "Yatsı:"]
+for i in range(6):
+    lbl.append(tk.Label(master = frm_namaz, text = str(vakitler[i])))
+    lbl[i].grid(row = (i), column = 0, padx = 10, sticky = "w")
 
-lbl11 = tk.Label(master = frm_vakitler, text = ":", font = "20")
-lbl22 = tk.Label(master = frm_vakitler, text = ":", font = "20")
-lbl33 = tk.Label(master = frm_vakitler, text = ":", font = "20")
-lbl44 = tk.Label(master = frm_vakitler, text = ":", font = "20")
-lbl55 = tk.Label(master = frm_vakitler, text = ":", font = "20")
-lbl66 = tk.Label(master = frm_vakitler, text = ":", font = "20")
+    lbl_.append(tk.Label(master = frm_namaz, text = "00:00"))
+    lbl_[i].grid(row = (i), column = 2, padx = 10, sticky = "w")
 
-lbl1.grid(row = 0, column = 0, padx = 10, sticky = "w")
-lbl2.grid(row = 1, column = 0, padx = 10, sticky = "w")
-lbl3.grid(row = 2, column = 0, padx = 10, sticky = "w")
-lbl4.grid(row = 3, column = 0, padx = 10, sticky = "w")
-lbl5.grid(row = 4, column = 0, padx = 10, sticky = "w")
-lbl6.grid(row = 5, column = 0, padx = 10, sticky = "w")
+# ----- ----- ----- ----- ----- HAVA DURUMU ----- ----- ----- ----- -----
+"""
+frm_hava = tk.Frame(master = window, relief = "ridge", borderwidth = 3)
+frm_hava.grid(row = 1, column = 1, padx = 10, pady = 10)
+saatler = ["Şimdi:", "Bugün:", "Yarın:"]
+for i in range(3):
+    lblA.append(tk.Label(master = frm_hava, text = str(saatler[i])))
+    lblA[i].grid(row = i, column = 0, padx = 10, sticky = "w")
 
-lbl11.grid(row = 0, column = 1, padx = 10, sticky = "w")
-lbl22.grid(row = 1, column = 1, padx = 10, sticky = "w")
-lbl33.grid(row = 2, column = 1, padx = 10, sticky = "w")
-lbl44.grid(row = 3, column = 1, padx = 10, sticky = "w")
-lbl55.grid(row = 4, column = 1, padx = 10, sticky = "w")
-lbl66.grid(row = 5, column = 1, padx = 10, sticky = "w")
+    lbl_A.append(tk.Label(master = frm_hava, text = "- \N{DEGREE CELSIUS}"))
+    lbl_A[i].grid(row = i, column = 1, padx = 10, sticky = "w")
+"""
+# ----- ----- ----- ----- ----- YENİLE BUTONU ----- ----- ----- ----- ----- 
+btn_yenile = tk.Button(
+    master = window, 
+    text = "Yenile", 
+    command = yenile,
+    font = "18")
+btn_yenile.grid(row = 2, column = 1, padx = 10, pady = 5, sticky = "e")
 
-frm_vakitler.grid(row = 0, column = 0, pady = 10)
-# frm_vakitler.pack()
+yenile()
 
-btn_yenile = tk.Button(master = window, text = "Yenile", command = yenile)
-btn_yenile.grid(row = 1, column = 0, sticky = "w", pady = 10, padx = 10)
-# btn_yenile.pack()
 window.mainloop()
